f1ffcea9005c119e1f45d5aa17b7e70f8132c8bd
Bruno Henrique <82820038+bruno535@users.noreply.github.com>
2021-09-22 21:35:15 -0300

    Add files via upload

---
+++ Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExemploApiCatalogoJogos.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException()
+            : base("Este jogo ja está cadastrado em nosso sistema")
+        { }
+    }
+}
